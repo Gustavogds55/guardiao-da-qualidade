03fa4e7ef5cfc149c34757f700fee4c57c5e0760
Gustavo <gustavogds.pt@gmail.com>
2025-09-03 12:14:38 +0100

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -8,11 +8,23 @@ describe('Teste de login', () => {
     wrongCredentialAlert: '.oxd-alert',
     dasboardGrid: ".orangehrm-dashboard-grid"
   }
+  const userData = {
+    userSuccess: {
+      username: 'Admin',
+      password: 'admin123'
+    },
+    userFail: {
+      username: 'Teste',
+      password: 'teste123'
+    }
+  }
+    
+
   
   it('Login com sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorslist.usernameField).type('Admin')
-    cy.get(selectorslist.passwordField).type('admin123')
+    cy.get(selectorslist.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorslist.passwordField).type(userData.userSuccess.password)
     cy.get(selectorslist.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorslist.dasboardGrid)
@@ -20,24 +32,24 @@ describe('Teste de login', () => {
   })
    it('Login com falha - usuario invalido', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorslist.usernameField).type('Teste')
-    cy.get(selectorslist.passwordField).type('admin123')
+    cy.get(selectorslist.usernameField).type(userData.userFail.username)
+    cy.get(selectorslist.passwordField).type(userData.userFail.password)
     cy.get(selectorslist.loginButton).click()
     cy.get(selectorslist.wrongCredentialAlert)
 
   })
   it('Login com falha - senha invalida', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorslist.usernameField).type('Admin')
-    cy.get(selectorslist.passwordField).type('Teste')
+    cy.get(selectorslist.usernameField).type(userData.userFail.username)
+    cy.get(selectorslist.passwordField).type(userData.userFail.password)
     cy.get(selectorslist.loginButton).click()
     cy.get(selectorslist.wrongCredentialAlert)
 
   })
    it('Login com falha - usuario e senha invalidos', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorslist.usernameField).type('ddddd')
-    cy.get(selectorslist.passwordField).type('wwwww')
+    cy.get(selectorslist.usernameField).type(userData.userFail.username)
+    cy.get(selectorslist.passwordField).type(userData.userFail.password)
     cy.get(selectorslist.loginButton).click()
     cy.get(selectorslist.wrongCredentialAlert)
 
