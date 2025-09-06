84e55f3afa57209d346347d1a8fa06a1aeb1d36d
Gustavo <gustavogds.pt@gmail.com>
2025-09-06 13:43:14 +0100

    Added new update for user spec

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,6 +1,6 @@
 import userData from '../fixtures/user-data.json'
 
-describe('Teste de login', () => {
+describe('User Info Update', () => {
 
   const selectorslist = {
     usernameField: '[name="username"]',
@@ -9,7 +9,15 @@ describe('Teste de login', () => {
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
     wrongCredentialAlert: '.oxd-alert',
     dasboardGrid: ".orangehrm-dashboard-grid",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]'
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameFild: '[name="firstName"]',
+    middldeNameFild: '[name="middleName"]',
+    lastNameField: '[name="lastName"]',
+    genericField: '.oxd-input--active',
+    dateField: "[placeholder='yyyy-dd-mm']",
+    closeButton: '.--close',
+    saveButton: '.orangehrm-left-space'
+
   }
 
   it('User Info Update - Success', () => {
@@ -20,6 +28,22 @@ describe('Teste de login', () => {
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorslist.dasboardGrid)
     cy.get(selectorslist.myInfoButton).click()
+    cy.get(selectorslist.firstNameFild).clear().type('Antonio')
+    cy.get(selectorslist.middldeNameFild).clear().type('Luiz')
+    cy.get(selectorslist.lastNameField).clear().type('Rodrigues')
+    cy.get(selectorslist.genericField).eq(3).clear().type('3782')
+    cy.get(selectorslist.genericField).eq(4).clear().type('5748')
+    cy.get(selectorslist.genericField).eq(5).clear().type('1111')
+    cy.get(selectorslist.dateField).eq(0).clear().type('2025-10-30')
+    cy.get(selectorslist.closeButton).click()
+    cy.get(selectorslist.saveButton).eq(0).click()
+    cy.get('.oxd-toast').should('contain', 'Successfully Updated')
+
+
+
+
+
+
 
 })
 
