5113f80a8300a218023a3b231e635c1a2d853539
Gustavo <gustavogds.pt@gmail.com>
2025-09-08 12:41:28 +0100

    Add page objects

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,47 +1,58 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/login.js'
+import DashboardPage from '../pages/dashbordPage.js'
+
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
 
 describe('Teste de login', () => {
 
   const selectorslist = {
-    usernameField: '[name="username"]',
-    passwordField: '[name="password"]',
-    loginButton: '.oxd-button',
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
-    wrongCredentialAlert: '.oxd-alert',
     dasboardGrid: ".orangehrm-dashboard-grid"
   }
 
-  it('Login com sucesso', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorslist.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorslist.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorslist.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorslist.dasboardGrid)
+  it.only('Login com sucesso', () => {
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+    dashboardPage.checkDashboardPage()
+    // cy.get(selectorslist.usernameField).type(userData.userSuccess.username)
+    // cy.get(selectorslist.passwordField).type(userData.userSuccess.password)
+    // cy.get(selectorslist.loginButton).click()
+    //cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    //cy.get(selectorslist.dasboardGrid)
 
   })
    it('Login com falha - usuario invalido', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorslist.usernameField).type(userData.userFail.username)
-    cy.get(selectorslist.passwordField).type(userData.userFail.password)
-    cy.get(selectorslist.loginButton).click()
-    cy.get(selectorslist.wrongCredentialAlert)
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password)
+    
+    // cy.visit('/auth/login')
+    // cy.get(selectorslist.usernameField).type(userData.userFail.username)
+    // cy.get(selectorslist.passwordField).type(userData.userFail.password)
+    // cy.get(selectorslist.loginButton).click()
+    // cy.get(selectorslist.wrongCredentialAlert)
 
   })
   it('Login com falha - senha invalida', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorslist.usernameField).type(userData.userFail.username)
-    cy.get(selectorslist.passwordField).type(userData.userFail.password)
-    cy.get(selectorslist.loginButton).click()
-    cy.get(selectorslist.wrongCredentialAlert)
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password)
+    // cy.visit('/auth/login')
+    // cy.get(selectorslist.usernameField).type(userData.userFail.username)
+    // cy.get(selectorslist.passwordField).type(userData.userFail.password)
+    // cy.get(selectorslist.loginButton).click()
+    //cy.get(selectorslist.wrongCredentialAlert)
 
   })
    it('Login com falha - usuario e senha invalidos', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorslist.usernameField).type(userData.userFail.username)
-    cy.get(selectorslist.passwordField).type(userData.userFail.password)
-    cy.get(selectorslist.loginButton).click()
-    cy.get(selectorslist.wrongCredentialAlert)
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password)
+    // cy.visit('/auth/login')
+    // cy.get(selectorslist.usernameField).type(userData.userFail.username)
+    // cy.get(selectorslist.passwordField).type(userData.userFail.password)
+    // cy.get(selectorslist.loginButton).click()
+    //cy.get(selectorslist.wrongCredentialAlert)
 
   })
   
